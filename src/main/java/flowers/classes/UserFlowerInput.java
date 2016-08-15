5db9bf08396f45ea895464790fcfe3bcae9136ff
Andrew Sidun <asidun@mail.ru>
2016-08-15 14:53:20 +0300

    Update UserFlowerInput.java

--- src/main/java/flowers/classes/UserFlowerInput.java
+++ src/main/java/flowers/classes/UserFlowerInput.java
@@ -1,8 +1,8 @@
 package src.main.java.flowers.classes;
 
 public class UserFlowerInput {
-    public String type;
-    public int length, lifetime, price;
+    public String type; // нужно сделать приватным, чтобы была инкапсуляция данных. Посмотри видео или почитай зачем нужна инкапсуляция.
+    public int length, lifetime, price; // нужно сделать приватным 
 
     public UserFlowerInput (String type, int length, int lifetime, int price) {
         this.type = type;
