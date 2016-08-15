7fa6359464685ccd82d1ccf5473fc900b14ed509
Andrew Sidun <asidun@mail.ru>
2016-08-15 15:00:27 +0300

    Update FlowerAbstract.java

--- src/main/java/flowers/classes/flower_architecture/FlowerAbstract.java
+++ src/main/java/flowers/classes/flower_architecture/FlowerAbstract.java
@@ -1,6 +1,7 @@
 package src.main.java.flowers.classes.flower_architecture;
 
-public abstract class FlowerAbstract {
+public abstract class FlowerAbstract { // лучше было сделать интерфейсом, т.к. тут нет реализованых методов. 
+//Мы можем реализовать несколько интерфейсов а унаследоваться только от одного класса
     protected int size = 0;
     protected int lifetime = 0;
     protected int price = 0;
